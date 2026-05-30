1ee16b0c1cd244f3ab751eb8d3042a6b6f9d8b2a
Arya Sinha <aryasinhaa2304@gmail.com>
2026-05-30 19:57:43 +0530

    Add MongoDB URI validation

--- college-platform/lib/mongodb.ts
+++ college-platform/lib/mongodb.ts
@@ -1,6 +1,10 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI!;
+const uri = process.env.MONGODB_URI;
+
+if (!uri) {
+  throw new Error("MONGODB_URI is not defined");
+}
 
 const client = new MongoClient(uri);
 
